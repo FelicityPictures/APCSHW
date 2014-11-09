3ce228c7a1b608049ac5bb58d885352f936d4475
FelicityPictures <felicitypictures@yahoo.com>
2014-11-09 15:57:27 -0600

    temp hiding:

--- orderedpairs.java
+++
@@ -1,9 +0,0 @@
-public class orderedpairs{
-    public static void main(String[]args){
-	value x,y;
-	x = new value();
-	y = new value();
-	x.coordinate();
-	y.coordinate();
-    }
-}
--- something.java
+++
@@ -1,9 +0,0 @@
-public class something{
-    int a,b;
-    public something(){
-    }
-
-    public void print(){
-        System.out.println(a+","+b);
-    }
-}
