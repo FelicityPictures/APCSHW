e7530b5994588510eb3a4926b4c5c6f8df806c59
FelicityPictures <felicitypictures@yahoo.com>
2014-11-24 00:27:25 -0600

    Attempted to use text and scanner

--- 03WordSearch/Driver.java
+++ 03WordSearch/Driver.java
@@ -1,9 +1,22 @@
-public class Driver{
+import java.util.*;
+import java.io.File;
+  public class Driver{
     public static void main(String[]args){
-	WordGrid x = new WordGrid(5,5);
-	System.out.println(x.toString());
 
-	x.addWord("meow",3,0);
-	System.out.println(x.toString());
+      //set up empty word search grid
+      WordGrid x = new WordGrid(5,5);
+      System.out.println(x.toString());
+
+      //add words list
+      File text = new File("APCSHW/03WordSearch/HappyWords.txt");
+      Scanner sc = new Scanner(text);
+      ArrayList<String>list = new ArrayList<String>();
+      for(int i=0;sc.hasNextLine();i++){
+        list.add(sc.nextLine());
+      }
+
+      //test it out
+      x.addWord("meow",3,0);
+      System.out.println(x.toString());
     }
-}
+  }
