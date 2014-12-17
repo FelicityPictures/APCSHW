942f73a41ea067981e3f10a822331af508a2d7d6
FelicityPictures <felicitypictures@yahoo.com>
2014-12-16 21:15:42 -0600

    Finished bubbleSort

--- 06Sorts/SuperArray.java
+++ 06Sorts/SuperArray.java
@@ -1,121 +1,125 @@
 public class SuperArray{
-    int size;
-    int[] data;
+  int size;
+  int[] data;
 
-    public SuperArray(){
-	this(10);
-    }
-
-    public SuperArray(int startCapacity){
-	size = 0;
-	data = new String[ startCapacity ];
-    }
-		
-    public String toString(){
-	String res = "[";
-	for(int i = 0; i < size; i++){
-	    res += data[i];
-	    if(i != size - 1){
-		res += " ";
-	    }
-	}
-	return res + "]";
-    }
-
-    public void add(int o){
-	if(size() == data.length){
-	    resize(size * 2);
-	}
-	data[ size ] = o;
-	size++;
-    }
-    public void add(int index, int o){
-	if(index < 0 || index > size() ){
-	    throw new IndexOutOfBoundsException();
-	}				
-	if(size() == data.length){
-	    resize( size * 2 );
-	}
-	for(int i = data.length - 1; i > index; i--){
-	    data[i] = data[i - 1];
-	}
-	data[ index ] = o;
-	size++;
-    }
-
-    public int size(){
-	return size;
-    }
-
-    public void clear(){
-	size = 0;
-    }
-
-    public void resize(int newCapacity){
-	String[] newData = new String[newCapacity];
-	int max = Math.min(newCapacity,size);
-	for(int i = 0; i < max; i++){
-	    newData[i]= data[i];
-	}
-	data = newData;
-    }
-		
-    public String set(int index, int o){
-	if(index < 0 || index >= size()){
-	    throw new IndexOutOfBoundsException();
-	}
-	String temp = data[index];
-	data[index]= o;
-	return temp;
-    }
-
-    public String get(int index){
-	if(index < 0 || index >= size()){
-	    throw new IndexOutOfBoundsException();
-	}
-	return data[index];
-    }
-
-    public String remove(int index){
-	if(index < 0 || index >= size()){
-	    throw new IndexOutOfBoundsException();
-	}
-
-	String temp = data[index];
-	while(index < size() - 1){
-	    data[index] = data[index + 1];
-	    index++;
-	}
-	size--;
-	if(size < data.length / 4){
-	    resize(data.length / 2);
-	}
-	return temp;
-    }
-
-  public void bubbleSort(){
-	for(int i=size;i>1;i--){
-	    if(data[i]>data[i-1]){
-		int m = data[i];
-		data[i] = data[i-1];
-		data[i-1]=m;
-	    }
-	}
+  public SuperArray(){
+    this(10);
   }
 
-    public static void main(String[]args){
-	SuperArray test = new SuperArray();
-	test.add(99);
-	test.add(6);
-	test.add(2);
-	test.add(203);
-	test.add(9);
-	test.add(32);
-	test.add(3);
-	test.add(84);
-	test.add(2);
-	System.out.println(test.toString());
-	test.bubbleSort();
-	System.out.println(test.toString());
+  public SuperArray(int startCapacity){
+    size = 0;
+    data = new int[ startCapacity ];
+  }
+		
+  public String toString(){
+    String res = "[";
+    for(int i = 0; i < size; i++){
+	    res += data[i];
+	    if(i != size - 1){
+        res += " ";
+	    }
     }
+    return res + "]";
+  }
+
+  public void add(int o){
+    if(size() == data.length){
+	    resize(size * 2);
+    }
+    data[ size ] = o;
+    size++;
+  }
+  public void add(int index, int o){
+    if(index < 0 || index > size() ){
+	    throw new IndexOutOfBoundsException();
+    }				
+    if(size() == data.length){
+	    resize( size * 2 );
+    }
+    for(int i = data.length - 1; i > index; i--){
+	    data[i] = data[i - 1];
+    }
+    data[ index ] = o;
+    size++;
+  }
+
+  public int size(){
+    return size;
+  }
+
+  public void clear(){
+    size = 0;
+  }
+
+  public void resize(int newCapacity){
+    int[] newData = new int[newCapacity];
+    int max = Math.min(newCapacity,size);
+    for(int i = 0; i < max; i++){
+	    newData[i]= data[i];
+    }
+    data = newData;
+  }
+		
+  public int set(int index, int o){
+    if(index < 0 || index >= size()){
+	    throw new IndexOutOfBoundsException();
+    }
+    int temp = data[index];
+    data[index]= o;
+    return temp;
+  }
+
+  public int get(int index){
+    if(index < 0 || index >= size()){
+	    throw new IndexOutOfBoundsException();
+    }
+    return data[index];
+  }
+
+  public int remove(int index){
+    if(index < 0 || index >= size()){
+	    throw new IndexOutOfBoundsException();
+    }
+
+    int temp = data[index];
+    while(index < size() - 1){
+	    data[index] = data[index + 1];
+	    index++;
+    }
+    size--;
+    if(size < data.length / 4){
+	    resize(data.length / 2);
+    }
+    return temp;
+  }
+
+  public void bubbleSort(){
+    for(int i=size-1;i>0;i--){
+      for(int w=0;w<i;w++){
+        if(data[w]>data[w+1]){
+          int m = data[w];
+          data[w]=data[w+1];
+          data[w+1]=m;
+        }
+      }
+    }
+  }
+
+  public static void main(String[]args){
+    SuperArray t = new SuperArray();
+    t.add(100);
+    t.add(9);
+    t.add(32);
+    t.add(3);
+    t.add(11);
+    t.add(4);
+    t.add(2);
+    t.add(0);
+    t.add(7);
+    t.add(19);
+    t.add(11);
+    System.out.println(t.toString());
+    t.bubbleSort();
+    System.out.println(t.toString());
+  }
 }
